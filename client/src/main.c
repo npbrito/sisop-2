a89e1a8f797b654da109d1407ca262509e748612
Leonardo Duarte <duartelb@pm.me>
2023-06-17 14:18:25 -0300

    Formatting

--- client/src/main.c
+++ client/src/main.c
@@ -18,7 +18,7 @@ int main(int argc, char* argv[argc+1])
     struct sockaddr_in servaddr;
     memset(&servaddr, 0, sizeof servaddr);
     servaddr.sin_family = AF_INET;
-	servaddr.sin_port   = htons(str_to_port(argv[2]));
+	servaddr.sin_port = htons(str_to_port(argv[2]));
     Inet_pton(AF_INET, argv[1], &servaddr.sin_addr);
     Connect(sockfd, (SA *) &servaddr, sizeof servaddr);
 
