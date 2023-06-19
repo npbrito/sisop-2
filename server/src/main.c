f7c516b24a5f24d8c446d528f890a56b8df285a9
Leonardo Duarte <duartelb@pm.me>
2023-06-19 12:20:42 -0300

    Configurable port in server

--- server/src/main.c
+++ server/src/main.c
@@ -5,17 +5,23 @@
 #include <string.h>
 #include <time.h>
 #include "error.h"
+#include "util.h"
 #include "wrapsock.h"
 #include "wrapunix.h"
 
-int main()
+int main(int argc, char* argv[argc+1])
 {
+    if (argc != 2) {
+        fprintf(stderr, "usage: ./myServer <port>\n");
+        return EXIT_FAILURE;
+    }
+
     int listenfd = Socket(AF_INET, SOCK_STREAM, 0);
     struct sockaddr_in servaddr;
     memset(&servaddr, 0, sizeof servaddr);
     servaddr.sin_family = AF_INET;
     servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-	servaddr.sin_port = htons(12345);
+	servaddr.sin_port = htons(str_to_port(argv[1]));
     Bind(listenfd, (SA *) &servaddr, sizeof servaddr);
     Listen(listenfd, LISTENQ);
 
---
+++ server/src/util.h
@@ -0,0 +1,8 @@
+#ifndef UTIL_H
+#define UTIL_H
+
+#include <stdint.h>
+
+uint16_t str_to_port(const char* str);
+
+#endif
